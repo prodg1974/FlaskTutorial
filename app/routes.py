e160a43160b181b4a1a962d87d11b7094ea2342c
Paul Rodgers <paul_rodg@yahoo.com>
2018-01-07 18:18:55 -0500

    Second Chapter Complete

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
     
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'paul','lastfour':'8888'}
+    posts = [
+        {
+            'author': {'username':'John'},
+            'body':'Beautiful day in New Orleans!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'Bills lost, oh well!'
+        }
+    ]
+    #return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title = 'Loop',user=user, posts=posts)
